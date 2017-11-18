2f4938e8c8603e5385f83c44aa85554b190cea2e
Samuel R. Djiani <djiasara87@yahoo.fr>
2017-11-18 06:02:36 -0500

    minor change

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ exports.TEST_DATABASE_URL = (
                               process.env.TEST_DATABASE_URL ||
                               'mongodb://localhost/univProfdb');
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET 
+exports.JWT_SECRET = process.env.JWT_SECRET || 'secret'
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY 
 
 exports.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID ;
